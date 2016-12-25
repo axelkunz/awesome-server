a26bd84459d5c3a3026947aebff998a160d7a4bc
SHANYUAN <shanyuan55@icloud.com>
2016-12-25 23:48:19 +0700

    fixed sort bug

--- src/app/shared/order-by.pipe.ts
+++ src/app/shared/order-by.pipe.ts
@@ -10,7 +10,7 @@ export class OrderByPipe implements PipeTransform {
         const property = arg1;
         const order = arg2 || "asc";
         // return _.orderBy(value, [user => user[property].toLowerCase()], [order]);
-        return _.orderBy(value, [property], [order]);
+        return _.orderBy(value, property, [order]);
     }
 
 }
